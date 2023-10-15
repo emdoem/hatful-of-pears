2d9acb2f54924f6e4b81a647842e5a059bd8a3ba
emdoem <125324546+emdoem@users.noreply.github.com>
2023-10-16 00:42:15 +0200

    fix form submission

--- components/InitializeCompetition.tsx
+++ components/InitializeCompetition.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
-import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { Select, SelectTrigger, SelectContent, SelectItem, SelectValue } from './ui/select';
 import { useForm } from 'react-hook-form';
@@ -10,19 +9,27 @@ import * as z from "zod";
 import { Form, FormLabel, FormField, FormItem, FormControl, FormDescription } from '@/components/ui/form'
 
 const FormSchema = z.object({
-  competitionType: z.string({
+  competitionType: z.enum(["fixed couples", "mixed couples", "solo"], {
     required_error: "Please select competition type."
   }),
   numberOfJudges: z.number().min(3, { message: "Competition needs at least 3 judges!" }),
   numberOfDancers: z.number().min(2, { message: "Competition needs at least 2 dancers / couples!" })
 })
 
-export function InitializeCompetition({ onSubmit }: { [prop: string]: any; }) {
+export function InitializeCompetition({ handleSubmit }: { [prop: string]: any; }) {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
+    defaultValues: {
+      numberOfJudges: 5,
+      numberOfDancers: 6
+    }
   })
 
-  // proper submit handler to be resolved
+  function onSubmit(values: z.infer<typeof FormSchema>) {
+    handleSubmit(values);
+    console.log(values);
+  }
+
   return (
     <Card>
       <CardHeader>
@@ -30,55 +37,56 @@ export function InitializeCompetition({ onSubmit }: { [prop: string]: any; }) {
       </CardHeader>
       <CardContent>
         <Form {...form}>
-          <FormField
-            control={form.control}
-            name="competitionType"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Competition type</FormLabel>
-                <Select>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="competitionType"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Competition type</FormLabel>
+                  <Select onValueChange={field.onChange}>
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Please select a competition type" {...field} />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="fixed couples">Fixed couples</SelectItem>
+                      <SelectItem value="mixed couples">Mixed couples</SelectItem>
+                      <SelectItem value="solo">Solo</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormDescription>For competitions with separate judging for leaders and followers please create separate solo competitions.</FormDescription>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="numberOfJudges"
+              render={({ field }) => (
+                <FormItem className="my-5">
+                  <FormLabel>Number of judges</FormLabel>
                   <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Please select a competition type" />
-                    </SelectTrigger>
+                    <Input type='number' min="3" {...field} />
                   </FormControl>
-                  <SelectContent>
-                    <SelectItem value="fixed couples">Fixed couples</SelectItem>
-                    <SelectItem value="mixed couples">Mixed couples</SelectItem>
-                    <SelectItem value="solo">Solo</SelectItem>
-                  </SelectContent>
-                </Select>
-                <FormDescription>For competitions with separate judging for leaders and followers please create separate solo competitions.</FormDescription>
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="numberOfJudges"
-            render={({ field }) => (
-              <FormItem className="my-5">
-                <FormLabel>Number of judges</FormLabel>
-                <FormControl>
-                  <Input type='number' min="3" />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="numberOfDancers"
-            render={({ field }) => (
-              <FormItem className="my-5">
-                <FormLabel>Number of couples / solo dancers</FormLabel>
-                <FormControl>
-                  <Input type='number' min="2" />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-          <Button onClick={onSubmit}>Create</Button>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="numberOfDancers"
+              render={({ field }) => (
+                <FormItem className="my-5">
+                  <FormLabel>Number of couples / solo dancers</FormLabel>
+                  <FormControl>
+                    <Input type='number' min="2" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Create</Button>
+          </form>
         </Form>
-
       </CardContent>
     </Card>
   );
