1173a768e38e338ce4071adba5c683cb9658cdc6
emdoem <125324546+emdoem@users.noreply.github.com>
2023-10-16 00:43:16 +0200

    export sample data

---
+++ lib/helper_functions/sampleDataForCompetitionCreator.tsx
@@ -0,0 +1,40 @@
+'use client';
+export const sampleDancers = [
+  {
+    id: 7,
+    dancers: 'Betty Takier, Hal Takier',
+    judge_A: 3,
+    judge_B: 1,
+    judge_C: 1
+  },
+  {
+    id: 13,
+    dancers: 'Natalia Esparza, Maxi Dorf',
+    judge_A: 1,
+    judge_B: 2,
+    judge_C: 3
+  },
+  {
+    id: 32,
+    dancers: 'Laila & Willie Desatoff',
+    judge_A: 2,
+    judge_B: 3,
+    judge_C: 2
+  }
+];
+export const sampleJudges = [
+  {
+    id: 'A',
+    firstName: 'Andreas',
+    lastName: 'Olsson'
+  },
+  {
+    id: 'B',
+    firstName: 'Jeremy',
+    lastName: 'Otth'
+  }, {
+    id: 'C',
+    firstName: 'Mickey',
+    lastName: 'Fortanasce'
+  }
+];
