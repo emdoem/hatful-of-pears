6daff6b5c2693682b3685548cd267294bbab33f8
emdoem <125324546+emdoem@users.noreply.github.com>
2023-10-19 02:32:03 +0200

    change ScoreTable styling

--- components/ScoreTable.tsx
+++ components/ScoreTable.tsx
@@ -6,28 +6,36 @@ import {
   TableHeader,
   TableRow
 } from "@/components/ui/table";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 export function ScoreTable({ data }: { data: {}[]; }) {
   const tableColumns = [...Object.keys(data[0])];
   return (
-    <Table className="my-3">
-      <TableHeader>
-        <TableRow>
-          {tableColumns.map((property) => (
-            <TableCell>{property}</TableCell>
-          ))}
-
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {data.map((dancer: any) => (
-          <TableRow>
-            {tableColumns.map((property) => (
-              <TableHead>{dancer[property]}</TableHead>
+    <Card>
+      <CardHeader>
+        <CardTitle>Table Title</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Table className="my-3">
+          <TableHeader>
+            <TableRow>
+              {tableColumns.map((property) => (
+                <TableCell>{property}</TableCell>
+              ))}
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.map((dancer: any) => (
+              <TableRow>
+                {tableColumns.map((property) => (
+                  <TableHead>{dancer[property]}</TableHead>
+                ))}
+              </TableRow>
             ))}
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+          </TableBody>
+        </Table>
+      </CardContent>
+
+    </Card>
   );
 }
