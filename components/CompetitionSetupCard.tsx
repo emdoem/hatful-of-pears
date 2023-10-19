181b06171a140092f0e310b51dc7c82b74d34449
emdoem <125324546+emdoem@users.noreply.github.com>
2023-10-19 02:31:08 +0200

    export component

---
+++ components/CompetitionSetupCard.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
+import { StartCompValues } from './CompetitionCreator';
+
+export function CompetitionSetupCard({
+  competitionSetup
+}: {
+  competitionSetup: StartCompValues;
+}) {
+  const { competitionType, numberOfJudges, numberOfDancers } = competitionSetup;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Competition Setup</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>
+          Competition type: {competitionType}<br />
+          number of judges: {numberOfJudges}<br />
+          number of couples / solo dancers: {numberOfDancers}
+        </p>
+      </CardContent>
+    </Card>
+  );
+}
