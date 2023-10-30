ae9c9d56f96ff7ce99a4b21d5841fbc7a9854b3c
emdoem <125324546+emdoem@users.noreply.github.com>
2023-10-31 00:21:15 +0100

    include majority condition for finalsResults

--- lib/finalsResults.ts
+++ lib/finalsResults.ts
@@ -6,36 +6,43 @@ import { removeDancerFromTable } from "./helper_functions/removeDancerFromTable"
 
 export function finalsResults(finalsScores: FinalsScoreTable) {
     let positionScoreTable: PositionScoreTable = scoresToTable(finalsScores);
-    aggregateScores(positionScoreTable);
+    positionScoreTable = aggregateScores(positionScoreTable);
 
     let results: FinalsResultsTable = [];
 
     for (let position = 0; position < positionScoreTable.length; position++) {
         let modeForPosition: number[] = findMode(positionScoreTable[position]);
-        let pointerForTies = position;
-
-        while (modeForPosition.length > 1) {
-            if (position === 1 && positionScoreTable[pointerForTies + 1]) {
-                modeForPosition = findMode(positionScoreTable[pointerForTies + 1].filter(id => modeForPosition.includes(id)));
-                pointerForTies++;
-            } else if (position > 1 && positionScoreTable[pointerForTies - 1]) {
-                modeForPosition = findMode(positionScoreTable[pointerForTies - 1]);
-                pointerForTies--;
-            } else {
-                break;
-            }
-        }
-
+        // majority condition?
         if (modeForPosition.length === 1) {
-            results[position] = modeForPosition[0];
-            positionScoreTable = removeDancerFromTable(results[position] as number, positionScoreTable);
-        } else {
-            const tiedDancers = modeForPosition; // this is bonkers, but there's a typing issue with using results[position]
-            results[position] = modeForPosition;
-            for (let j = 0; j < tiedDancers.length; j++) {
-                positionScoreTable = removeDancerFromTable(tiedDancers[j], positionScoreTable);
+            const occurenceCount = positionScoreTable[position].filter(dancer => modeForPosition.includes(dancer)).length;
+            if (occurenceCount > positionScoreTable[0].length / 2) {
+                results[position] = modeForPosition[0];
+                positionScoreTable = removeDancerFromTable(results[position] as number, positionScoreTable);
+            } else {
+                let pointerForTies = position;
+                do {
+                    if (position === 1 && positionScoreTable[pointerForTies + 1]) {
+                        modeForPosition = findMode(positionScoreTable[pointerForTies + 1].filter(id => modeForPosition.includes(id)));
+                        pointerForTies++;
+                    } else if (position > 1 && positionScoreTable[pointerForTies - 1]) {
+                        modeForPosition = findMode(positionScoreTable[pointerForTies - 1]);
+                        pointerForTies--;
+                    } else {
+                        break;
+                    }
+                } while (modeForPosition.length > 1);
+                if (modeForPosition.length === 1) {
+                    results[position] = modeForPosition[0];
+                    positionScoreTable = removeDancerFromTable(results[position] as number, positionScoreTable);
+                } else {
+                    const tiedDancers = modeForPosition; // this is bonkers, but there's a typing issue with using results[position]
+                    results[position] = modeForPosition;
+                    for (let j = 0; j < tiedDancers.length; j++) {
+                        positionScoreTable = removeDancerFromTable(tiedDancers[j], positionScoreTable);
+                    }
+        
+                }
             }
-
         }
     }
 
