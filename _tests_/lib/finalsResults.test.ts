2ae6341d9d4d8cca307ab58f641c77fee8ccad93
emdoem <125324546+emdoem@users.noreply.github.com>
2023-11-19 22:16:00 +0100

    further work on business logic

--- _tests_/lib/finalsResults.test.ts
+++ _tests_/lib/finalsResults.test.ts
@@ -2,75 +2,8 @@ import { describe, xdescribe, expect, it, beforeEach } from '@jest/globals';
 import { finalsResults } from '../../lib/finalsResults';
 import { FinalsResultsTable } from '@/lib/types';
 
-describe('finalsResults ', () => {
-    describe('for a sample score table ', () => {
-        const inputScores = [
-            {
-                id: 'judge_A',
-                scores: {
-                    firstPlace: 7,
-                    secondPlace: 3,
-                    thirdPlace: 1
-                }
-            },
-            {
-                id: 'judge_B',
-                scores: {
-                    firstPlace: 7,
-                    secondPlace: 4,
-                    thirdPlace: 2
-                }
-            },
-            {
-                id: 'judge_C',
-                scores: {
-                    firstPlace: 7,
-                    secondPlace: 5,
-                    thirdPlace: 1
-                }
-            },
-            {
-                id: 'judge_D',
-                scores: {
-                    firstPlace: 3,
-                    secondPlace: 2,
-                    thirdPlace: 7
-                }
-            },
-            {
-                id: 'judge_E',
-                scores: {
-                    firstPlace: 1,
-                    secondPlace: 6,
-                    thirdPlace: 5
-                }
-            },
-        ];
-        const results = finalsResults(inputScores);
-
-        it('returns a winner with a clear majority', () => {
-            const expectedWinner = 7;
-
-            const winner = results[0];
-
-            expect(winner).toStrictEqual(expectedWinner);
-        });
-        it('breaks the tie for second place', () => {
-            const expected = 3;
-
-            const outcome = results[1];
-
-            expect(outcome).toStrictEqual(expected);
-        });
-        it('breaks the tie for third place', () => {
-            const expected = 1;
-
-            const outcome = results[2];
-
-            expect(outcome).toStrictEqual(expected);
-        });
-    });
-    describe('for a random score table', () => {
+describe('finalsResults ', () => {       
+    describe('for a random score table I', () => {
         const inputScores = [
             {
                 id: 'A',
@@ -136,8 +69,228 @@ describe('finalsResults ', () => {
             const winner = results[0];
 
             expect(winner).toStrictEqual(expected);
-        })
+        });
+        it('assigns 3rd place to dancer no 3', () => {
+            const expected = 3;
 
+            const thirdPlace = results[2];
+
+            expect(thirdPlace).toStrictEqual(expected);
+        });
+        it('assigns 4th place to dancer no 4', () => {
+            const expected = 4;
+
+            const fourthPlace = results[3];
+
+            expect(fourthPlace).toStrictEqual(expected);
+        });
+        it('assigns 5th place to dancer no 6', () => {
+            const expected = 6;
+
+            const fifthPlace = results[4];
+
+            expect(fifthPlace).toStrictEqual(expected);
+        });
+
+    })
+    describe('for a random score table II', () => {
+        const inputScores = [
+            {
+                id: 'A',
+                scores: {
+                    '1st': 2,
+                    '2nd': 4,
+                    '3rd': 1,
+                    '4th': 5,
+                    '5th': 6,
+                    '6th': 3
+                }
+            },
+            {
+                id: 'B',
+                scores: {
+                    '1st': 2,
+                    '2nd': 6,
+                    '3rd': 5,
+                    '4th': 4,
+                    '5th': 1,
+                    '6th': 3
+                }
+            },
+            {
+                id: 'C',
+                scores: {
+                    '1st': 6,
+                    '2nd': 4,
+                    '3rd': 1,
+                    '4th': 2,
+                    '5th': 3,
+                    '6th': 5
+                }
+            },
+            {
+                id: 'D',
+                scores: {
+                    '1st': 6,
+                    '2nd': 1,
+                    '3rd': 2,
+                    '4th': 5,
+                    '5th': 3,
+                    '6th': 4
+                }
+            },
+            {
+                id: 'E',
+                scores: {
+                    '1st': 4,
+                    '2nd': 3,
+                    '3rd': 5,
+                    '4th': 6,
+                    '5th': 1,
+                    '6th': 2
+                }
+            },
+        ];
+        const results = finalsResults(inputScores);
+
+        it('breaks the tie for 1st place', () => {
+            const expected = 6;
+
+            const winner = results[0];
+
+            expect(winner).toStrictEqual(expected);
+        });
+        it('assigns 2nd place to dancer no 4', () => {
+            const expected = 4;
+
+            const secondPlace = results[1];
+
+            expect(secondPlace).toStrictEqual(expected);
+        });
+        it('assigns 3rd place to dancer no 2', () => {
+            const expected = 2;
+
+            const thirdPlace = results[2];
+
+            expect(thirdPlace).toStrictEqual(expected);
+        });
+        it('assigns 4th place to dancer no 1', () => {
+            const expected = 1;
+
+            const fourthPlace = results[3];
+
+            expect(fourthPlace).toStrictEqual(expected);
+        });
+        it('assigns 5th place to dancer no 5', () => {
+            const expected = 5;
+
+            const fifthPlace = results[4];
+
+            expect(fifthPlace).toStrictEqual(expected);
+        });
+        it('assigns 6th place to dancer no 3', () => {
+            const expected = 3;
+
+            const sixthPlace = results[5];
+
+            expect(sixthPlace).toStrictEqual(expected);
+        });
+    }) 
+    describe('for a random score table III', () => {
+        const inputScores = [
+            {
+                id: 'judge_A',
+                scores: {
+                    '1st': 106,
+                    '2nd': 102,
+                    '3rd': 104,
+                    '4th': 103,
+                    '5th': 105,
+                    '6th': 101
+                }
+            },
+            {
+                id: 'judge_B',
+                scores: {
+                    '1st': 101,
+                    '2nd': 104,
+                    '3rd': 105,
+                    '4th': 103,
+                    '5th': 106,
+                    '6th': 102
+                }
+            },
+            {
+                id: 'judge_C',
+                scores: {
+                    '1st': 104,
+                    '2nd': 102,
+                    '3rd': 101,
+                    '4th': 105,
+                    '5th': 103,
+                    '6th': 106
+                }
+            },
+            {
+                id: 'judge_D',
+                scores: {
+                    '1st': 104,
+                    '2nd': 103,
+                    '3rd': 106,
+                    '4th': 101,
+                    '5th': 102,
+                    '6th': 105
+                }
+            },
+            {
+                id: 'judge_E',
+                scores: {
+                    '1st': 104,
+                    '2nd': 101,
+                    '3rd': 103,
+                    '4th': 102,
+                    '5th': 106,
+                    '6th': 105
+                }
+            }
+        ];
+        const results: FinalsResultsTable = finalsResults(inputScores);
+
+        it('assigns 1st place to dancer no 104', () => {
+            const expected = 104;
+
+            const firstPlace = results[0];
+
+            expect(firstPlace).toStrictEqual(expected);
+        });
+        it('assigns 2nd place to dancer no 101', () => {
+            const expected = 101;
+
+            const secondPlace = results[1];
+
+            expect(secondPlace).toStrictEqual(expected);
+        });
+        it('assigns 3rd place to dancer no 102', () => {
+            const expected = 102;
+
+            const thirdPlace = results[2];
+
+            expect(thirdPlace).toStrictEqual(expected);
+        });
+        it('assigns 4th place to dancer no 103', () => {
+            const expected = 103;
+
+            const fourthPlace = results[3];
+
+            expect(fourthPlace).toStrictEqual(expected);
+        });
+        it('assigns 5th place to dancer no 106', () => {
+            const expected = 106;
+
+            const fifthPlace = results[4];
+
+            expect(fifthPlace).toStrictEqual(expected);
+        });
     })
     describe('for a historical data sample I ', () => {
         const inputScores = [
--- lib/finalsResults.ts
+++ lib/finalsResults.ts
@@ -4,51 +4,160 @@ import { aggregateScores } from "./helper_functions/aggregateScores";
 import { scoresToTable } from "./helper_functions/scoresToTable";
 import { removeDancerFromTable } from "./helper_functions/removeDancerFromTable";
 
-export function finalsResults(finalsScores: FinalsScoreTable) {
-    let positionScoreTable: PositionScoreTable = scoresToTable(finalsScores);
-    positionScoreTable = aggregateScores(positionScoreTable);
+type ScoresTable = DancerScores[];
+type DancerScores = {
+    id: string,
+    scores: number[],
+    calculated?: number,
+    position?: number
+}
+type DancerScoresCalculated = {
+    id: string,
+    scores: number[],
+    calculated: number,
+    position?: number
+}
 
+function calculateScores(
+    dancerScores: DancerScores, 
+    position: number
+) {
+    dancerScores.calculated = dancerScores.scores.reduce(
+        (accumulator, currentValue) => (currentValue <= position) ? ++accumulator : accumulator, 
+        0
+    );
+    return dancerScores;
+}
+
+function findDancersForPosition(calculatedScoresTable: DancerScoresCalculated[]) {
+    const maxCalculated = Math.max(...calculatedScoresTable.map(dancer => dancer.calculated), 0);
+    return calculatedScoresTable
+        .filter(dancer => dancer.calculated === maxCalculated)
+        // .map(dancer => dancer.id);
+}
+
+export function recursiveResults(
+    scoresTable: ScoresTable,
+    results: string[][] = []
+) {
+    if (scoresTable.length > 0) { // base condition
+        const position = results.flat().length + 1;
+
+        // this is to be refactored into a single function?
+        let calculatedScoresTable = scoresTable.map(dancer => calculateScores(dancer, position));
+        let dancerForPosition = findDancersForPosition(calculatedScoresTable as DancerScoresCalculated[]);
+
+        if (dancerForPosition.length === 1) {
+            // majorityCheck  
+            if (dancerForPosition[0].calculated < dancerForPosition[0].scores.length / 2) {
+                // check next position
+                calculatedScoresTable = scoresTable.map(dancer => calculateScores(dancer, position + 1));
+                dancerForPosition = findDancersForPosition(calculatedScoresTable as DancerScoresCalculated[]);
+                if (dancerForPosition.length > 1) {
+                    // check the score sum
+                    // update dancerForPosition
+                } // continue
+            } // continue to push
+        } else {
+            // tie for position
+            if (position === 1) {
+                calculatedScoresTable = scoresTable.map(dancer => calculateScores(dancer, position + 1));
+                dancerForPosition = findDancersForPosition(calculatedScoresTable as DancerScoresCalculated[]);                
+            } else {
+                // check the score sum
+                // update dancerForPosition
+            }
+        }
+
+        results.push(dancerForPosition.map(dancer => dancer.id)); 
+        const scoresTableForNextPos = scoresTable
+            .filter(dancer => {
+                // filter out placed dancer(s)
+                const placedDancers = results.flat();
+                return (!placedDancers.includes(dancer.id));
+            })
+            
+        recursiveResults(scoresTableForNextPos, results);
+    }
+    return results;
+}
+
+// previous implementation was a mess, starting over
+export function finalsResults(finalsScores: FinalsScoreTable) {
+    // setup
+    let positionScoreTable: PositionScoreTable = aggregateScores(scoresToTable(finalsScores));
     let results: FinalsResultsTable = [];
 
+    function updateTable(placedDancer: number, position: number) {
+        results[position] = placedDancer;
+        positionScoreTable = removeDancerFromTable(placedDancer, positionScoreTable);
+    }
+
+    // start of iterative loop (for each position)
     for (let position = 0; position < positionScoreTable.length; position++) {
-        let modeForPosition: number[] = findMode(positionScoreTable[position]);
-        // majority condition?
+        const modeForPosition: number[] = findMode(positionScoreTable[position]);
+
         if (modeForPosition.length === 1) {
-            const occurenceCount = positionScoreTable[position].filter(dancer => modeForPosition.includes(dancer)).length;
-            if (occurenceCount > positionScoreTable[0].length / 2) {
-                results[position] = modeForPosition[0];
-                positionScoreTable = removeDancerFromTable(results[position] as number, positionScoreTable);
-            } else {
-                let pointerForTies = position;
-                do {
-                    if (position === 1 && positionScoreTable[pointerForTies + 1]) {
-                        modeForPosition = findMode(positionScoreTable[pointerForTies + 1].filter(id => modeForPosition.includes(id)));
-                        pointerForTies++;
-                    } else if (position > 1 && positionScoreTable[pointerForTies - 1]) {
-                        modeForPosition = findMode(positionScoreTable[pointerForTies - 1]);
-                        pointerForTies--;
-                    } else {
-                        break;
-                    }
-                } while (modeForPosition.length > 1);
-                if (modeForPosition.length === 1) {
-                    results[position] = modeForPosition[0];
-                    positionScoreTable = removeDancerFromTable(results[position] as number, positionScoreTable);
+            // majority condition  
+            if (checkMajority(positionScoreTable[position], modeForPosition[0])) { // majority
+                updateTable(modeForPosition[0], position); // this good
+            } else { // no majority                
+                const modeForNext = findMode(positionScoreTable[position + 1]);
+                if (modeForNext.length === 1) {
+                    updateTable(modeForNext[0], position); // this good
                 } else {
-                    const tiedDancers = modeForPosition; // this is bonkers, but there's a typing issue with using results[position]
-                    results[position] = modeForPosition;
-                    for (let j = 0; j < tiedDancers.length; j++) {
-                        positionScoreTable = removeDancerFromTable(tiedDancers[j], positionScoreTable);
+                    updateTable(modeForPosition[0], position); // this good
+                }
+            }
+        } else {
+            // tie on position 
+            if (position > 0) {
+                // tie for 2nd place & higher
+                const modeForPrev = findMode(positionScoreTable[position - 1]);
+                if (modeForPrev.length === 1) {
+                    updateTable(modeForPrev[0], position); // this good
+                } else {
+                    // tie
+                    results[position] = modeForPrev;
+                    for (let j = 0; j < modeForPrev.length; j++) {
+                        positionScoreTable = removeDancerFromTable(modeForPrev[j], positionScoreTable);
                     }
-        
+                    position++;
+                }
+            } else {
+                // tie for 1st place
+                const modeForNext = findMode(positionScoreTable[position + 1]);
+                if (modeForNext.length === 1) { // this is the issue
+                    updateTable(modeForNext[0], position);
+                } else {
+                    // tie
+                    if (modeForNext === modeForPosition) {
+                        results[position] = modeForPosition;
+                        for (let j = 0; j < modeForPosition.length; j++) {
+                            positionScoreTable = removeDancerFromTable(modeForPosition[j], positionScoreTable);
+                        }
+                        position++;
+                    } else {
+                        const tieWinner = modeForNext.filter((dancer: number) => modeForPosition.includes(dancer))[0];
+                        const tieLoser = modeForNext.filter((dancer: number) => dancer !== tieWinner)[0];
+                        updateTable(tieWinner, position);
+                        updateTable(tieLoser, position + 1);
+                        position++;
+                    }
+
                 }
             }
         }
     }
 
+    // end of loop
     return results;
 }
 
-function isArrayOfNumbers(arr: number | number[]): arr is number[] {
-    return Array.isArray(arr);
+function checkMajority(arrayOfNumbers: number[], modeOfArray: number): boolean {
+    const occurenceCount = arrayOfNumbers.filter(number => number === modeOfArray).length;
+    if (occurenceCount > arrayOfNumbers.length / 2) {
+        return true;
+    }
+    return false;
 }
