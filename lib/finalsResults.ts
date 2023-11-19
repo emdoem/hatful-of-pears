4f5a65c724a797dd796fed99f92b21d04b566d68
emdoem <125324546+emdoem@users.noreply.github.com>
2023-11-20 00:15:21 +0100

    new logic for finalsResults

--- lib/finalsResults.ts
+++ lib/finalsResults.ts
@@ -29,135 +29,77 @@ function calculateScores(
     return dancerScores;
 }
 
-function findDancersForPosition(calculatedScoresTable: DancerScoresCalculated[]) {
+function scoreSum(
+    dancerScores: DancerScoresCalculated, 
+    position: number
+) {
+    dancerScores.calculated = dancerScores.scores.reduce(
+        (accumulator, currentValue) => (currentValue <= position) ? accumulator + currentValue : accumulator,
+        0
+    );
+    return dancerScores;
+}
+
+function findSmallestSum(tiedForPosition: DancerScoresCalculated[]) {
+    const minValue = tiedForPosition.reduce(
+        (minValue, current) => (current.calculated < minValue) ? current.calculated : minValue,
+        Infinity
+    )
+    return tiedForPosition.filter(dancer => dancer.calculated === minValue);
+}
+
+function findDancersForPosition(scoresTable: ScoresTable, position: number) {
+    const calculatedScoresTable: DancerScoresCalculated[] = scoresTable.map(dancer => calculateScores(dancer, position) as DancerScoresCalculated);
     const maxCalculated = Math.max(...calculatedScoresTable.map(dancer => dancer.calculated), 0);
     return calculatedScoresTable
         .filter(dancer => dancer.calculated === maxCalculated)
         // .map(dancer => dancer.id);
 }
 
-export function recursiveResults(
+function filterOutPlacedDancers(scoresTable: ScoresTable, results: string[][]) {
+    return scoresTable
+        .filter(dancer => {
+            // filter out placed dancer(s)
+            const placedDancers = results.flat();
+            return (!placedDancers.includes(dancer.id));
+        })
+}
+
+export function finalsResults(
     scoresTable: ScoresTable,
     results: string[][] = []
 ) {
     if (scoresTable.length > 0) { // base condition
         const position = results.flat().length + 1;
-
-        // this is to be refactored into a single function?
-        let calculatedScoresTable = scoresTable.map(dancer => calculateScores(dancer, position));
-        let dancerForPosition = findDancersForPosition(calculatedScoresTable as DancerScoresCalculated[]);
+        let dancerForPosition = findDancersForPosition(scoresTable, position);
 
         if (dancerForPosition.length === 1) {
             // majorityCheck  
             if (dancerForPosition[0].calculated < dancerForPosition[0].scores.length / 2) {
                 // check next position
-                calculatedScoresTable = scoresTable.map(dancer => calculateScores(dancer, position + 1));
-                dancerForPosition = findDancersForPosition(calculatedScoresTable as DancerScoresCalculated[]);
+                dancerForPosition = findDancersForPosition(scoresTable, position + 1);
                 if (dancerForPosition.length > 1) {
                     // check the score sum
+                    const tiedForPosition = dancerForPosition.map(dancer => scoreSum(dancer, position));                    
                     // update dancerForPosition
+                    dancerForPosition = findSmallestSum(tiedForPosition);
                 } // continue
-            } // continue to push
+            } // continue to results.push()
         } else {
             // tie for position
             if (position === 1) {
-                calculatedScoresTable = scoresTable.map(dancer => calculateScores(dancer, position + 1));
-                dancerForPosition = findDancersForPosition(calculatedScoresTable as DancerScoresCalculated[]);                
+                dancerForPosition = findDancersForPosition(scoresTable, position + 1);                
             } else {
                 // check the score sum
+                const tiedForPosition = dancerForPosition.map(dancer => scoreSum(dancer, position));                    
                 // update dancerForPosition
+                dancerForPosition = findSmallestSum(tiedForPosition);
             }
         }
 
         results.push(dancerForPosition.map(dancer => dancer.id)); 
-        const scoresTableForNextPos = scoresTable
-            .filter(dancer => {
-                // filter out placed dancer(s)
-                const placedDancers = results.flat();
-                return (!placedDancers.includes(dancer.id));
-            })
-            
-        recursiveResults(scoresTableForNextPos, results);
+        const scoresTableForNextPos = filterOutPlacedDancers(scoresTable, results);            
+        finalsResults(scoresTableForNextPos, results);
     }
     return results;
-}
-
-// previous implementation was a mess, starting over
-export function finalsResults(finalsScores: FinalsScoreTable) {
-    // setup
-    let positionScoreTable: PositionScoreTable = aggregateScores(scoresToTable(finalsScores));
-    let results: FinalsResultsTable = [];
-
-    function updateTable(placedDancer: number, position: number) {
-        results[position] = placedDancer;
-        positionScoreTable = removeDancerFromTable(placedDancer, positionScoreTable);
-    }
-
-    // start of iterative loop (for each position)
-    for (let position = 0; position < positionScoreTable.length; position++) {
-        const modeForPosition: number[] = findMode(positionScoreTable[position]);
-
-        if (modeForPosition.length === 1) {
-            // majority condition  
-            if (checkMajority(positionScoreTable[position], modeForPosition[0])) { // majority
-                updateTable(modeForPosition[0], position); // this good
-            } else { // no majority                
-                const modeForNext = findMode(positionScoreTable[position + 1]);
-                if (modeForNext.length === 1) {
-                    updateTable(modeForNext[0], position); // this good
-                } else {
-                    updateTable(modeForPosition[0], position); // this good
-                }
-            }
-        } else {
-            // tie on position 
-            if (position > 0) {
-                // tie for 2nd place & higher
-                const modeForPrev = findMode(positionScoreTable[position - 1]);
-                if (modeForPrev.length === 1) {
-                    updateTable(modeForPrev[0], position); // this good
-                } else {
-                    // tie
-                    results[position] = modeForPrev;
-                    for (let j = 0; j < modeForPrev.length; j++) {
-                        positionScoreTable = removeDancerFromTable(modeForPrev[j], positionScoreTable);
-                    }
-                    position++;
-                }
-            } else {
-                // tie for 1st place
-                const modeForNext = findMode(positionScoreTable[position + 1]);
-                if (modeForNext.length === 1) { // this is the issue
-                    updateTable(modeForNext[0], position);
-                } else {
-                    // tie
-                    if (modeForNext === modeForPosition) {
-                        results[position] = modeForPosition;
-                        for (let j = 0; j < modeForPosition.length; j++) {
-                            positionScoreTable = removeDancerFromTable(modeForPosition[j], positionScoreTable);
-                        }
-                        position++;
-                    } else {
-                        const tieWinner = modeForNext.filter((dancer: number) => modeForPosition.includes(dancer))[0];
-                        const tieLoser = modeForNext.filter((dancer: number) => dancer !== tieWinner)[0];
-                        updateTable(tieWinner, position);
-                        updateTable(tieLoser, position + 1);
-                        position++;
-                    }
-
-                }
-            }
-        }
-    }
-
-    // end of loop
-    return results;
-}
-
-function checkMajority(arrayOfNumbers: number[], modeOfArray: number): boolean {
-    const occurenceCount = arrayOfNumbers.filter(number => number === modeOfArray).length;
-    if (occurenceCount > arrayOfNumbers.length / 2) {
-        return true;
-    }
-    return false;
 }
