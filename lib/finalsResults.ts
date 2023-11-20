967a5524dbda10cd1d62d0cbcd8c922cc8a365bf
emdoem <125324546+emdoem@users.noreply.github.com>
2023-11-20 12:15:53 +0100

    fix minor bugs in logic

--- lib/finalsResults.ts
+++ lib/finalsResults.ts
@@ -63,8 +63,12 @@ export function finalsResults(
     scoresTable: ScoresTable,
     results: string[][] = []
 ) {
+    let tieLoser: DancerScoresCalculated[] = []; // will have to find a way to get rid of it
     if (scoresTable.length > 0) { // base condition
         const position = results.flat().length + 1;
+        // if 2 couples get tied for a position - leave the next position blank i.e.: 1st place: couples 101 & 103, 3rd place: couple 107 and no 2nd place
+        if (results[results.length - 1] && results[results.length - 1].length > 1) results.push([]); // this works (for now, unless I get 3 couples tied for a position)
+
         let dancerForPosition = findDancersForPosition(scoresTable, position);
 
         if (dancerForPosition.length === 1) {
@@ -82,16 +86,25 @@ export function finalsResults(
         } else {
             // tie for position
             if (position === 1) {
-                dancerForPosition = findDancersForPosition(scoresTable, position + 1);                
+                dancerForPosition = findDancersForPosition(scoresTable, position + 1);   
+                if (dancerForPosition.length > 1) {
+                    // check the score sum
+                    const tiedForPosition = dancerForPosition.map(dancer => scoreSum(dancer, position + 1));                    
+                    // update dancerForPosition
+                    dancerForPosition = findSmallestSum(tiedForPosition);
+                } // continue             
             } else {
                 // check the score sum
                 const tiedForPosition = dancerForPosition.map(dancer => scoreSum(dancer, position));                    
                 // update dancerForPosition
                 dancerForPosition = findSmallestSum(tiedForPosition);
+                // assign tie loser to next position
+                tieLoser = tiedForPosition.filter(dancer => !dancerForPosition.includes(dancer));
             }
         }
 
         results.push(dancerForPosition.map(dancer => dancer.id)); 
+        if (tieLoser.length > 0) results.push([tieLoser[0].id]); // this is suboptimal, looking for a work around
         const scoresTableForNextPos = filterOutPlacedDancers(scoresTable, results);            
         results = finalsResults(scoresTableForNextPos, results);
     }
