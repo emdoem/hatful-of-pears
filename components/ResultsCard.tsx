4e846f6971d5ee4e70a66436d1b0534e75b3f878
emdoem <125324546+emdoem@users.noreply.github.com>
2023-11-27 14:28:32 +0100

    delete a comma

--- components/ResultsCard.tsx
+++ components/ResultsCard.tsx
@@ -43,7 +43,7 @@ export function ResultsCard({
                   {dancer},
                   <br />
                 </React.Fragment>
-              ))},
+              ))}
             </p>
           ))}
         </CardContent>
